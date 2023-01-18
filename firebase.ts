3e1d1fcb0864eac66c3e60cd3935dafac5093d15
Laura Mejía Celis <lauram2celis@gmail.com>
2023-01-18 07:49:38 -0500

    starting firebase connection

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDybMMUIgYFYHQm-qRcf2l2uuzBpEBFbFA",
+  authDomain: "netflix-clone-ff854.firebaseapp.com",
+  projectId: "netflix-clone-ff854",
+  storageBucket: "netflix-clone-ff854.appspot.com",
+  messagingSenderId: "730114980762",
+  appId: "1:730114980762:web:2551283f416505b733d823"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
