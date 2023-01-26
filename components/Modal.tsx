847845e560e2e5dbe3736269f9cdf23bcd38bc42
Laura Mejía Celis <lauram2celis@gmail.com>
2023-01-26 17:28:43 -0500

    modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,31 @@
 import { XCircleIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import MuiModal from '@mui/material/Modal'
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
+import { Movie } from '../typings';
 
 const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(()=>{
+    if(!movie)return
+
+    async function fetchMovie() {
+      const data = await fetch(`https://api.themoviedb.org/3/${
+          movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`).then((res)=>res.json())
+
+        console.log(data)
+
+      // if(data?.videos){
+      //   const index = data.videos.results.findIndex()
+      // }
+    }
+  },[movie])
 
   const handleClose = ()=>{
     setShowModal(false)
@@ -15,10 +36,14 @@ const Modal = () => {
       <>
         <button
           onClick={handleClose}
-          className='modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818]'>
+          className='modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]'>
           
           <XMarkIcon className='h-6 w-6'/>
         </button>
+
+        <div>
+
+        </div>
       </>
       
     </MuiModal>
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { baseUrl } from "../constants/movies";
 import { Movie } from "../typings";
+import { useRecoilState } from "recoil";
 
 interface Props {
   // When using firebase
@@ -9,6 +10,11 @@ interface Props {
 }
 
 const Thumbnail = ({movie} : Props) => {
+  //const [showModal, setShowModal] = useRecoilState(modalState)
+  //const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+//1.44
+
+
   return ( 
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
       <Image 
@@ -16,6 +22,9 @@ const Thumbnail = ({movie} : Props) => {
       alt=''
       className="rounded-sm object-cover md:rounded"
       layout="fill"
+      // onClick={()=>{
+      //       setCurrentMovie(movie)
+      //       setShowModal(true)}}
       />
     </div>
   );
